f1f0476cac3f5412c2514d1cb146aa3d7467df7e
akiraishihara3 <akiraishihara1218@gmail.com>
2026-09-13 16:34:37 +0900

    content: add seasonal quick access actions

--- site-data.js
+++ site-data.js
@@ -18,6 +18,32 @@ window.SITE_DATA = {
   heroPrimaryImagePosition: 'center center',
   themeVisual: 'assets/micro-values-header.webp',
   originalHeroAssetPath: 'assets/micro-values-header.png',
+
+  primaryActionLabel: 'EXHIBITOR ENTRY',
+  primaryActionUrl: '#exhibit',
+  primaryActionStatus: 'EXHIBITORS NOW OPEN',
+  quickAccessLead: '目的からすぐに探す',
+  quickAccess: [
+    {
+      label: 'VISIT',
+      jp: '来場する',
+      description: '開催日程・会場・アクセスなど、次回開催情報を見る',
+      url: '#visit'
+    },
+    {
+      label: 'EXHIBIT',
+      jp: '出展する',
+      description: '37th MONTAGE 出展情報・募集概要を見る',
+      url: '#exhibit'
+    },
+    {
+      label: 'ONLINE',
+      jp: 'オンライン展示会',
+      description: '会場の展示プロダクトをオンラインでも見る',
+      url: 'https://howdi-exhibition.com/'
+    }
+  ],
+
   schedule: [
     { date: '2.24 WED', time: '10:00 A.M - 7:00 P.M' },
     { date: '2.25 THU', time: '10:00 A.M - 7:00 P.M' },
